8eb06aee250b5964b7390e7f283f13984f3f636a
abaranetsky <shurik0711@rambler.ru>
2022-12-17 15:58:06 +0200

    foru commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <img src="php.jpg" width="600   " height="290" alt="Наш логотип" class="logo" />
 </div>
 
-    <h1 aria-grabbed="true">Добро пожаловать на наш сайт!</h1>
+    <h1 aria-grabbed="false">Добро пожаловать на наш сайт!</h1>
 <?php
 include "test.inc.php";
 ?>
